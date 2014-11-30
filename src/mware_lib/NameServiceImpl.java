212566f0d680267688a8a7d354b9897a20796353
timotin <w.timon@gmail.com>
2014-12-01 00:10:17 +0100

    NameServiceImpl

---
+++ src/mware_lib/NameServiceImpl.java
@@ -0,0 +1,17 @@
+package mware_lib;
+
+public class NameServiceImpl extends NameService{
+
+	@Override
+	public void rebind(Object servant, String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object resolve(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
