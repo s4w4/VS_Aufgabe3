0d657ad9ddc4ae58f0958e00f8e3a10a15fe2fa7
timotin <w.timon@gmail.com>
2014-12-01 22:13:13 +0100

    Nameservice: Reference + Connection

--- src/name_service/NameService.java
+++ src/name_service/NameService.java
@@ -4,8 +4,23 @@ package name_service;
  *  name_service.NameService
  */
 public interface NameService extends Runnable{
-    public void rebind(String name, Reference reference);
-
+	
+   /**
+    * ein Objekt (servant) anmelden.
+    * Eine eventuell schon vorhandene Objektreferenz gleichen Namens
+    * wird überschrieben.
+    * 
+    * @param name
+    * @param reference
+    */
+	public void rebind(String name, Reference reference);
+   /**
+    *
+    * Liefert eine generische Objektreferenz zu einem Namen.
+    *
+    * @param name  String
+    * @return  Object
+    */
     public Reference resolve(String name);
 
      public void shutDown();
