5d633940580d5a24f5f500c30881c5b39ea784ed
timotin <w.timon@gmail.com>
2014-12-02 00:07:49 +0100

    Logger

--- src/name_service/Main.java
+++ src/name_service/Main.java
@@ -3,6 +3,7 @@ package name_service;
 public class Main {
 	public static void main(String[] args) {
 		int port = 5000; 
-		NameService ns = NameService.init(port);
+		NameServiceImpl ns = NameService.init(port);
+		ns.rebind("a",null);
 	}
 }
--- src/name_service/NameServiceImpl.java
+++ src/name_service/NameServiceImpl.java
@@ -1,15 +1,36 @@
 package name_service;
+import java.io.IOException;
+import java.util.logging.*;
 
 public class NameServiceImpl extends NameService {
 
+	private int port;
+	private Logger logger; 
+	private FileHandler fileHandler; 
+	
 	public NameServiceImpl(int port) {
-		// TODO Auto-generated constructor stub
+		try {
+			fileHandler = new FileHandler("log/Nameservice.log");
+			SimpleFormatter simpleFormatter = new SimpleFormatter(); 
+			fileHandler.setFormatter(simpleFormatter);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+
+		logger = Logger.getLogger(NameServiceImpl.class.getName() );
+		logger.addHandler(fileHandler);
+		logger.info("Logger erstellt");
+		this.port = port; 
+		
 	}
 
 	@Override
 	public void rebind(String name, Reference reference) {
 		// TODO Auto-generated method stub
-		
+
+		logger.info("rebind");
 	}
 
 	@Override
@@ -18,10 +39,5 @@ public class NameServiceImpl extends NameService {
 		return null;
 	}
 
-	@Override
-	public void shutDown() {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
