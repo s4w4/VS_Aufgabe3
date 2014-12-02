c47315868544c184a9a37315e848e13b31310150
sawa112 <AlexanderSawadski@gmail.com>
2014-12-02 05:05:58 +0100

    Dispatcher

--- src/mware_lib/Dispatcher.java
+++ src/mware_lib/Dispatcher.java
@@ -1,6 +1,7 @@
 package mware_lib;
 
+import java.io.IOException;
+
 public interface Dispatcher {
-
-
+    public Object sendToSkeleton(Reference reference, String methodName,Class<?>[] types, Object[] args) throws IOException, ClassNotFoundException;
 }
